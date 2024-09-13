adde898404a09046412daacc3a249a3609f93934
Isha Tiwari <85096427+IshaT-Hub@users.noreply.github.com>
2024-09-13 09:28:23 +0000

    new

--- 02/index.js
+++ 02/index.js
@@ -1,26 +1,19 @@
-const fs = require("fs");
+
 const http = require("http");
-const url = require("url");
 
-const myServer = http.createServer((req,res)=>{
-    if(req.url === '/favicon.ico') return res.end();
-    const log = `${Date.now()}: ${req.url} New req recieved\n`
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
-    fs.appendFile("log.txt", log, (err, data)=>{
-        switch (myUrl.pathname) {
-            case "/": res.end("This is home page")
-                 break;
-            case "/About": 
-                const username = myUrl.query.myName;
-                console.log(username);
-                
-                res.end(`Hi, ${username}`);
-                 break;
-            default: res.end("Error 404");
-        }
+const express = require("express");
 
-    });
+const app = express();
+
+app.get("/", (req,res)=>{
+    return res.send("HOME PAGE")
 });
+
+app.get("/About", (req,res)=>{
+    return res.send("About PAGE" + 'Hey' + req.query.Name);
+});
+
+
+const myServer = http.createServer(app);
 
 myServer.listen(8000, () => {console.log("SERVER STARTED")});
