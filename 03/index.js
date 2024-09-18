0b338a50705756ee7ca3222f3f7a6839f52f8449
IshaT-Hub <85096427+IshaT-Hub@users.noreply.github.com>
2024-09-18 20:21:01 +0530

    new

--- 03/index.js
+++ 03/index.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const users = require('./MOCK_DATA.json');
+const fs = require('fs');
 
 const app = express();  //instance
 
 const PORT = 8000;
 
-//Route
-// GET users - List all users 
-app.get('/api/users', (req, res) => {      // renders json data ( hybrid_server)
-    return res.json(users);
-})
+//middleware - plugin of name urlencoded || whenever we recieve form data it will put it in body
+app.use(express.urlencoded({extended:false})); 
 
-// GET /api/users/1 - List  user with id 1 (with id part dynamic)
-app.get('/api/users/:id', (req, res) => {
+// urlencoded ==> it did was, fetch data -> made its JS object -> Put it in req body
+
+//Route
+
+app.route('/api/users/:id').get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
-    return res.json(user);
+    return res.json(user);})
+    .patch((req, res)=>{
+        return res.json({status: "Pending"});
+    })
+    .delete((req, res)=>{
+        return res.json({status : "Pending"});
+    })
+
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;                // anything we send data from frontend, express makes it availabe in this body
+    users.push({...body, id: users.length+1});
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data)=>{
+        return res.json({status:"Success", id: users.length});
+    });
+    
 }); 
 
 app.listen(PORT, ()=> console.log(`SERVER STARTED at Port ${PORT}`));
